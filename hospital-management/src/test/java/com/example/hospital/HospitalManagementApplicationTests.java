ecc423e70f5398da602cdf8f6f77853d194db0f3
Abdessamad Belkhialat Hammouri <87809821+abde7h@users.noreply.github.com>
2024-10-16 12:57:45 +0200

    Add files via upload

---
+++ hospital-management/src/test/java/com/example/hospital/HospitalManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = HospitalManagementApplication.class)
+public class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test para cargar el contexto
+	}
+}
+
